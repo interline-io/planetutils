8d511aae3d049e3bbb7d6dcc050e164a4982fb30
Ian Rees <ian@ianrees.net>
2018-02-14 14:13:07 -0800

    extract bbox, fixes

--- scripts/planet.py
+++ scripts/planet.py
@@ -8,10 +8,6 @@ import urllib2
 
 import boto3
 
-def get_planet(bucket=None):
-    bucket = bucket or 'osm-pds'
-    return 'planet-latest.osm.pbf'
-
 class PlanetBase(object):
     def __init__(self, osmpath=None, grain='hour', changeset_url=None):
         self.osmpath = osmpath
@@ -41,6 +37,7 @@ class PlanetBase(object):
             '--out-timestamp'
         )
         if 'invalid' in timestamp:
+            print "no timestamp; falling back to osmconvert --out-statistics"
             statistics = self.osmconvert(
                 self.osmpath,
                 '--out-statistics'
@@ -51,33 +48,70 @@ class PlanetBase(object):
             ][0]
         return timestamp
 
-    def download_planet(self, outpath):
+    def download_planet(self):
         raise NotImplementedError
 
     def update_planet(self, outpath, grain='hour', changeset_url=None):
         raise NotImplementedError
 
+    def extract_bboxes(self, bboxes, workers=1):
+        args = []
+        args += ['--read-pbf-fast', self.osmpath, 'workers=%s'%int(workers)]
+        args += ['--tee', str(len(bboxes))]
+        for name,top,left,bottom,right in bboxes:
+            arg = [
+                '--bounding-box',
+                'top=%0.5f'%float(top),
+                'left=%0.5f'%float(left),
+                'bottom=%0.5f'%float(bottom),
+                'right=%0.5f'%float(right),
+                '--write-pbf',
+                '%s.osm.pbf'%name
+            ]
+            args += arg
+        self.osmosis(*args)
+
+    def extract_bboxes_csv(self, csvpath):
+        self.extract_bboxes(self._load_bboxes(csvpath))
+
+    def _load_bboxes(self, csvpath):
+        # bbox csv format:
+        # name, top, left, bottom, right
+        if not os.path.exists(csvpath):
+            raise Exception('csvpath does not exist: %s'%csvpath)
+        bboxes = []
+        with open(csvpath) as f:
+            reader = csv.reader(f)
+            for row in reader:
+                bboxes.append(row)
+        return bboxes
+
+class Planet(PlanetBase):
+    pass
+
 class PlanetDownloaderHttp(PlanetBase):
     pass
 
 class PlanetDownloaderS3(PlanetBase):
-    def download_planet(self, outpath):
-        self.download_planet_latest(outpath)
+    def download_planet(self):
+        self.download_planet_latest()
 
-    def download_planet_latest(self, outpath, bucket=None, prefix=None, match=None):
+    def download_planet_latest(self, bucket=None, prefix=None, match=None):
         match = match or '.*(planet[-_:T0-9]+.osm.pbf)$'
         bucket = bucket or 'osm-pds'
         objs = self._get_planets(bucket, prefix, match)
         objs = sorted(objs, key=lambda x:x.key)
         for i in objs:
-            print i
+            print "found planet: s3://%s/%s"%(i.bucket_name, i.key)
         planet = objs[-1]
-        self._download(planet.bucket_name, planet.key, outpath)
+        if os.path.exists(self.osmpath):
+            raise Exception('planet file exists: %s'%self.osmpath)
+        print "downloading: s3://%s/%s to %s"%(bucket_name, key, self.osmpath)
+        self._download(planet.bucket_name, planet.key)
 
-    def _download(self, bucket_name, key, outpath):
-        print "downloading: s3://%s/%s to %s"%(bucket_name, key, outpath)
+    def _download(self, bucket_name, key):
         s3 = boto3.client('s3')
-        s3.download_file(bucket_name, key, outpath)
+        s3.download_file(bucket_name, key, self.osmpath)
 
     def _get_planets(self, bucket, prefix, match):
         r = re.compile(match)
@@ -94,6 +128,8 @@ class PlanetUpdaterOsmupdate(PlanetBase):
 
 class PlanetUpdaterOsmosis(PlanetBase):
     def update_planet(self, outpath, grain='hour', changeset_url=None):
+        if not os.path.exists(self.osmpath):
+            raise Exception('planet file does not exist: %s'%self.osmpath)
         self.changeset_url = changeset_url or 'http://planet.openstreetmap.org/replication/%s'%grain
         self._initialize()
         self._initialize_state()
@@ -148,11 +184,3 @@ class PlanetUpdaterOsmosis(PlanetBase):
             '--write-pbf',
             outpath
         )
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('osmpath', help='osm path')
-    parser.add_argument('outpath', help='output path')
-    args = parser.parse_args()
-    p = PlanetUpdaterOsmosis(args.osmpath)
-    p.update_planet(args.outpath)
