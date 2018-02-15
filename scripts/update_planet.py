6dc9dbb4a79635324bb625d5ff900e47fb52d173
Ian Rees <ian@ianrees.net>
2018-02-14 16:41:05 -0800

    use --s3

--- scripts/update_planet.py
+++ scripts/update_planet.py
@@ -6,10 +6,14 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('osmpath', help='osm path')
     parser.add_argument('outpath', help='output path')
+    parser.add_argument('--s3', action='store_true', help='download using s3 from osm-pds')
     args = parser.parse_args()
     if not os.path.exists(args.osmpath):
         print "planet does not exist; downloading"
-        d = PlanetDownloaderS3(args.osmpath)
-        d.download_planet_latest()
+        if args.s3:
+            d = PlanetDownloaderS3(args.osmpath)
+        else:
+            d = PlanetDownloaderHttp(args.osmpath)
+        d.download_planet()
     p = PlanetUpdaterOsmosis(args.osmpath)
     p.update_planet(args.outpath)
