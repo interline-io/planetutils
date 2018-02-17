33125c310c44840fada69bbdfbc6b0ee994072ad
Ian Rees <ian@ianrees.net>
2018-02-16 18:29:06 -0800

    test rig

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-docker run --rm -v $HOME/data/planets:/app -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} -it osm-planet-update
+docker run --rm -v $HOME/data/planets:/data -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} -it osm-planet-update "$@"
 
