30730e6b670eff8d519edb9c590108eae0cb6307
Ian Rees <ian@ianrees.net>
2018-02-17 20:09:04 -0800

    improved bbox utils

--- planetutils/bbox.py
+++ planetutils/bbox.py
@@ -2,18 +2,27 @@
 import os
 import csv
 
-def load_bboxes(csvpath):
+def validate_bbox(bbox):
+    left, bottom, right, top = map(float, bbox)
+    assert -180 <= left <= 180
+    assert -180 <= right <= 180
+    assert -90 <= bottom <= 90
+    assert -90 <= top <= 90
+    assert top >= bottom
+    assert right >= left
+    return [left, bottom, right, top]
+
+def bbox_string(bbox):
+    return validate_bbox(bbox.split(','))
+
+def load_bboxes_csv(csvpath):
     # bbox csv format:
     # name, left, bottom, right, top
     if not os.path.exists(csvpath):
         raise Exception('csvpath does not exist: %s'%csvpath)
-    bboxes = []
+    bboxes = {}
     with open(csvpath) as f:
         reader = csv.reader(f)
         for row in reader:
-            name = row[0]
-            left, bottom, right, top = map(float, row[1:])
-            assert top >= bottom
-            assert right >= left
-            bboxes.append([name, left, bottom, right, top])
+            bboxes[row[0]] = validate_bbox(row[1:])
     return bboxes
