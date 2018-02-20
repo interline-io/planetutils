2a874576590e02dfa8225a0b60dd8125609ae8ae
Ian Rees <ian@ianrees.net>
2018-02-19 19:03:45 -0800

    more tests

---
+++ tests/test_elevation.py
@@ -0,0 +1,60 @@
+import tempfile
+import os
+import types
+import unittest
+
+import planetutils.elevation as elevation
+
+CA = [-126.386719,32.157012,-113.532715,42.244785]
+
+class TestElevationDownloader(unittest.TestCase):
+    def test_download_bboxes(self):
+        pass
+        
+    def test_hgtpath(self):
+        e = elevation.ElevationDownloader('.')
+        expect = ('N122', 'N122E037.hgt')
+        hgtpath = e.hgtpath(37, 122)
+        self.assertEquals(hgtpath[0], expect[0])
+        self.assertEquals(hgtpath[1], expect[1])
+    
+    def test_get_bbox_tiles(self):
+        e = elevation.ElevationDownloader('.')
+        tiles = e.get_bbox_tiles(CA)
+        self.assertEquals(len(tiles), 154)
+        tiles = e.get_bbox_tiles([-180,-90,180,90])
+        self.assertEquals(len(tiles), 64800)
+    
+    def download_bbox(self, e, method, args, expect):
+        COUNT = []
+        def c(self, bucket, prefix, x, y):
+            COUNT.append([x,y])
+        e.download_hgt = types.MethodType(c, elevation.ElevationDownloader)
+        method(*args)
+        self.assertEquals(len(COUNT), expect)
+    
+    def test_download_planet(self):
+        e = elevation.ElevationDownloader('.')
+        self.download_bbox(e, e.download_planet, [], 64800)
+    
+    def test_download_bbox(self):
+        e = elevation.ElevationDownloader('.')
+        self.download_bbox(e, e.download_bbox, [CA], 154)
+    
+    def test_download_bbox_found(self):
+        d = tempfile.mkdtemp()
+        e = elevation.ElevationDownloader(d)
+        # correct size
+        path = e.hgtpath(-119, 37)
+        os.makedirs(os.path.join(d, path[0]))
+        dp = os.path.join(d, *path)
+        with open(dp, 'w') as f:
+            f.write('0'*e.HGT_SIZE)
+        # incorrect size
+        path = e.hgtpath(-119, 36)
+        os.makedirs(os.path.join(d, path[0]))
+        dp = os.path.join(d, *path)
+        with open(dp, 'w') as f:
+            f.write('0')        
+        # expect 154 - 1
+        self.download_bbox(e, e.download_bbox, [CA], 154-1)
--- tests/test_planet.py
+++ tests/test_planet.py
@@ -1,9 +1,43 @@
+import tempfile
+import os
 import unittest
-import planetutils
+import planetutils.planet as planet
+
+TESTFILE = os.path.join('.','examples','san-francisco-downtown.osm.pbf')
+TESTFILE_TIMESTAMP = '2018-02-02T22:34:43Z'
+TEST_BBOX = [-122.430439,37.766508,-122.379670,37.800052]
 
 class TestPlanetBase(unittest.TestCase):
+    def test_osmosis(self):
+        p = planet.PlanetBase(TESTFILE)
+        output = p.osmosis(
+            '--read-pbf', TESTFILE,
+            '--tf', 'accept-ways', 'highway=pedestrian',
+            '--write-xml','-'
+        )
+        self.assertTrue(output.count('way id=') > 0)
+    
+    def test_osmconvert(self):
+        p = planet.PlanetBase(TESTFILE)
+        output = p.osmconvert(p.osmpath, '--out-statistics')
+        self.assertIn('timestamp min:', output)
+    
     def test_get_timestamp(self):
-        pass
+        p = planet.PlanetBase(TESTFILE)
+        self.assertEquals(p.get_timestamp(), TESTFILE_TIMESTAMP)
 
+    def test_extract_bbox(self, name=None, bbox=None):
+        name = name or 'test'
+        bbox = bbox or TEST_BBOX
+        d = tempfile.mkdtemp()
+        p = planet.PlanetBase(TESTFILE)
+        outfile = os.path.join(d, '%s.osm.pbf'%name)
+        p.extract_bbox(name, bbox, outpath=d)
+        self.assertTrue(os.path.exists(outfile))
+        p2 = planet.PlanetBase(outfile)
+        self.assertEquals(p2.get_timestamp(), TESTFILE_TIMESTAMP)
+        os.unlink(outfile)
+        os.rmdir(d)
+        
 if __name__ == '__main__':
     unittest.main()
