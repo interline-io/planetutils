c403c1feda561edccf87c18683c431d6536c6055
Ian Rees <ian@ianrees.net>
2018-05-02 16:06:30 -0700

    Extract bboxes defined in geojson file

--- planetutils/bbox.py
+++ planetutils/bbox.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import json
 import os
 import csv
 
@@ -19,10 +20,36 @@ def load_bboxes_csv(csvpath):
     # bbox csv format:
     # name, left, bottom, right, top
     if not os.path.exists(csvpath):
-        raise Exception('csvpath does not exist: %s'%csvpath)
+        raise Exception('file does not exist: %s'%csvpath)
     bboxes = {}
     with open(csvpath) as f:
         reader = csv.reader(f)
         for row in reader:
             bboxes[row[0]] = validate_bbox(row[1:])
     return bboxes
+
+def load_bboxes_geojson(path):
+    if not os.path.exists(path):
+        raise Exception('file does not exist: %s'%path)
+    with open(path) as f:
+        data = json.load(f)
+    return feature_bboxes(data.get('features',[]))
+
+def feature_bboxes(features):
+    bboxes = {}
+    for count,feature in enumerate(features):
+        key = feature.get('properties',{}).get('id') or feature.get('id') or count
+        bbox = feature_bbox(feature)
+        bboxes[key] = bbox
+    return bboxes
+
+def feature_bbox(feature):
+    g = feature.get('geometry',{})
+    if g.get('type') != 'Polygon':
+        raise Exception('Only Polygon geometries are supported')
+    coords = g.get('coordinates',[])[0]
+    lons = [i[0] for i in coords]
+    lats = [i[1] for i in coords]
+    left, right = min(lons), max(lons)
+    bottom, top = min(lats), max(lats)
+    return validate_bbox([left, bottom, right, top])
--- planetutils/osm_planet_extract.py
+++ planetutils/osm_planet_extract.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import argparse
 from planet import *
+import bbox
 from bbox import bbox_string, load_bboxes_csv
 
 def main():
@@ -8,16 +9,24 @@ def main():
     parser.add_argument('osmpath', help='Name or path to OSM planet file. Use planet_update if you do not have a copy locally.')
     parser.add_argument('--outpath', help='Extract output directory', default='.')
     parser.add_argument('--csv', help='Path to CSV file with bounding box definitions.')
+    parser.add_argument('--geojson', help='Path to GeoJSON file: bbox for each feature is extracted.')
     parser.add_argument('--name', help='Name to give to extract file.')
     parser.add_argument('--bbox', help='Bounding box for extract file. Format for coordinates: left,bottom,right,top')
     args = parser.parse_args()
     p = Planet(args.osmpath)
+
+    bboxes = {}
     if args.csv:
-        p.extract_bboxes(load_bboxes_csv(args.csv), outpath=args.outpath)
+        bboxes = bbox.load_bboxes_csv(args.csv)        
+    elif args.geojson:
+        bboxes = bbox.load_bboxes_geojson(args.geojson)
     elif (args.bbox and args.name):
-        p.extract_bbox(args.name, bbox_string(args.bbox), outpath=args.outpath)
+        bboxes[args.name] = bbox.bbox_string(args.bbox)
     else:
-        parser.error('must specify --csv or --bbox and --name')
+        parser.error('must specify --csv, --geojson, or --bbox and --name')
+    print bboxes
+
+    p.extract_bboxes(bboxes, outpath=args.outpath)
 
 if __name__ == '__main__':
     main()
