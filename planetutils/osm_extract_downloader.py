03ce60c792bfed8044454683e2e731540e535d6e
Ian Rees <ian@ianrees.net>
2018-09-04 15:20:21 -0700

    data_format

--- planetutils/osm_extract_downloader.py
+++ planetutils/osm_extract_downloader.py
@@ -1,33 +1,33 @@
 from __future__ import absolute_import, unicode_literals
 from future.standard_library import install_aliases
 install_aliases()
-from urllib.parse import urlparse, urlencode
+from urllib.parse import urlparse, urlencode, urlsplit, parse_qs, urlunsplit
 from urllib.request import urlopen
 
 import subprocess
 import json
-
 
 from . import log
 from . import download
 
 class OsmExtractDownloader(object):
     HOST = 'https://app.interline.io'
-    def download(self, outpath, osm_extract_id, osm_extract_version='latest', api_token=None):
+    def download(self, outpath, osm_extract_id, osm_extract_version='latest', data_format='pbf', api_token=None):
         # Endpoint
         url = '%s/osm_extracts/%s/download'%(self.HOST, osm_extract_version)
         if osm_extract_version == 'latest':
             url = '%s/osm_extracts/download_latest'%(self.HOST)
 
         # Make url
-        u = list(urlparse.urlsplit(url))
-        q = urlparse.parse_qs(u[3])
+        u = list(urlsplit(url))
+        q = parse_qs(u[3])
+        q['data_format'] = data_format
         if osm_extract_version == "latest":
             q['string_id'] = osm_extract_id
         if api_token:
             q['api_token'] = api_token
         u[3] = urlencode(q)
-        url = urlparse.urlunsplit(u)
+        url = urlunsplit(u)
 
         # Download
         download.download_curl(url, outpath)
